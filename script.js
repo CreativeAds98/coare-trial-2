34a834a91cf6e8560da7fb418d4ff6d01e568991
CreativeAds <Creative@gmail.com>
2026-03-02 10:43:11 +0530

    added scroll up arrow to all

--- script.js
+++ script.js
@@ -14,3 +14,25 @@
             navMenu.classList.remove('active');
         });
     });
+
+
+
+
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+    // Show button when user scrolls down 400px
+    window.onscroll = function() {
+        if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+            scrollTopBtn.style.display = "block";
+        } else {
+            scrollTopBtn.style.display = "none";
+        }
+    };
+
+    // Smooth scroll to top when clicked
+    scrollTopBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
